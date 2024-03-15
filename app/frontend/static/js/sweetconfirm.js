830e3897f193170b5eefb1fa8a88fd710ee9b974
adam-woolhether <114398282+adam-woolhether@users.noreply.github.com>
2024-03-15 21:19:29 +0800

    example hiddne

--- app/frontend/static/js/sweetconfirm.js
+++ app/frontend/static/js/sweetconfirm.js
@@ -1,3 +1,5 @@
+// couldn't get this to work
+/*
 function sweetConfirm(elt, config) {
     Swal.fire(config)
         .then((result) => {
@@ -6,3 +8,13 @@ function sweetConfirm(elt, config) {
             }
         });
 }
+
+*/
+/*                    <button type="button" class="bad bg color border"
+                            hx-delete="/contacts" hx-target="body" hx-trigger="confirmed"
+                            @click="sweetConfirm($el,
+                                    { title: 'Delete these contacts?',
+                                      showCancelButton: true,
+                                      confirmButtonText: 'Delete'})">
+                                      Delete
+                    </button>*/
