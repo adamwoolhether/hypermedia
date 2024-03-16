3c2f93d3cc859608f2bc4875eb5eea07b4440913
adamwoolhether <adamwoolhether@gmail.com>
2024-03-16 16:12:48 +0800

    fix sweet alert using external js issue, but still not working for single delete

--- app/hypermedia/frontend/static/js/sweetconfirm.js
+++ app/hypermedia/frontend/static/js/sweetconfirm.js
@@ -1,5 +1,4 @@
 // couldn't get this to work
-/*
 function sweetConfirm(elt, config) {
     Swal.fire(config)
         .then((result) => {
@@ -9,12 +8,18 @@ function sweetConfirm(elt, config) {
         });
 }
 
-*/
-/*                    <button type="button" class="bad bg color border"
-                            hx-delete="/contacts" hx-target="body" hx-trigger="confirmed"
-                            @click="sweetConfirm($el,
-                                    { title: 'Delete these contacts?',
-                                      showCancelButton: true,
-                                      confirmButtonText: 'Delete'})">
-                                      Delete
-                    </button>*/
+// Alternative, but less clean way, is to not use above JS and do it directly in html:
+/*
+                    <button type="button" class="bad bg color border"
+                        @click="Swal.fire({
+                                title: 'Delete these contacts?',
+                                showCancelButton: true,
+                                confirmButtonText: 'Delete'
+                            }).then(result => {
+                                if (result.isConfirmed) {
+                                    htmx.ajax('DELETE', '/contacts', { source: $root, target: document.body })
+                                }
+                            });"
+                        >Delete
+                    </button>
+                    */
