1d745a8f3cad07a9a2e0b3d58d00a27b1fb53951
adamwoolhether <adamwoolhether@gmail.com>
2024-03-18 14:55:32 +0800

    handle 400 for edit

--- app/hypermedia/handlers/contactsgrp/contactsgrp.go
+++ app/hypermedia/handlers/contactsgrp/contactsgrp.go
@@ -212,7 +212,7 @@ func (h *Handlers) Update(ctx context.Context, w http.ResponseWriter, r *http.Re
 			Email:     fieldErrs.Fields()["email"],
 		}
 
-		return web.RenderHTML(ctx, w, fe.EditByID(uc), http.StatusOK)
+		return web.RenderHTML(ctx, w, fe.EditFields(uc), http.StatusBadRequest)
 	}
 
 	err = h.core.Update(ctx, uc.ToDB())
